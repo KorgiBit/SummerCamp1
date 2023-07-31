fd9f539ada02976076ae5c6691c99aa99d4cae20
noble <noble@STEVEN>
2023-07-31 16:43:25 +0200

    Добавьте файлы проекта.

---
+++ SummerCamp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit warrior1 = new Unit("Вася", 100, 20, 40, Factions.Good, false);
+            Unit warrior2 = new Unit("Викинг", 200, 100, 10, Factions.Good, true);
+
+
+            warrior1.Attack(warrior2);
+            warrior2.Attack(warrior1);
+        }
+    }
+
+    
+}
---
+++ SummerCamp1/Unit.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace SummerCamp1
+{
+    internal class Unit
+    {
+        private readonly string _name;
+        private float _neutralAttackMultiplier = 1;
+        private float _friendlyAttackMultiplier = 0.5f;
+        private float _enemyAttackMultiplier = 1.5f;
+
+        private float _berserkarmorMultiplier = 0.2f;
+
+
+        private int _attackPower { get; set; }
+        private int _armor { get; set; }
+        private float _health;
+
+        private bool _isBerserk;
+        private Factions Faction;
+
+        public Unit(string name, float heath, int attackPower, int armor, Factions faction, bool isBerserk = false)
+        {
+            _name = name;
+            _health = heath;
+            _attackPower = attackPower;
+            _armor = armor;
+            Faction = faction;
+            _isBerserk = isBerserk;
+        }
+
+
+        public void Attack(Unit enemy)
+        {
+            TakeDamage(_attackPower, enemy);
+        }
+
+        private void TakeDamage(float attackPower, Unit attacker)
+        {
+            if (attackPower <= 0)
+            {
+                return;
+            }
+            float berserkMultiplier =  _isBerserk ? 2 : 1;
+            
+
+
+            float damageReduction = _armor / 100.0f;
+            if (_isBerserk)
+            {
+                damageReduction *= _berserkarmorMultiplier;
+            }
+
+            float actualDamage = attackPower * berserkMultiplier * CalculateFractionMultiplier( attacker) * (1 - damageReduction);
+            _health -= actualDamage;
+
+            Console.WriteLine(_name + " получил урон : " + actualDamage);
+            Console.WriteLine(_name + " осталось хп : " + _health);
+
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Сила атаки: ", _attackPower);
+            Console.WriteLine("здоровье: ", _health);
+            Console.WriteLine("броня: ", _armor);
+        }
+
+        private float CalculateFractionMultiplier(Unit attacker)
+        {
+            if (attacker.Faction == Factions.Neutral)
+            {
+                return _neutralAttackMultiplier;
+            } else if (attacker.Faction == Faction) {
+                
+                return _friendlyAttackMultiplier;
+            } else
+            {
+                
+                return _enemyAttackMultiplier;
+            }
+            
+        }
+    }
+}
